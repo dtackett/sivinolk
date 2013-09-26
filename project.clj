1f1fca78d9be665d5d170e0762ac647a67d27fa8
Devon Tackett <devon.tackett@gmail.com>
2013-09-25 22:20:44 -0700

    Now specifying a cljs version.

--- project.clj
+++ project.clj
@@ -6,12 +6,13 @@
   :source-paths ["src/clj"]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1896"]
                  [compojure "1.1.5"]]
 
   ;; lein-cljsbuild plugin to build a CLJS project
   :plugins [
   			;; cljsbuild plugin
-  			[lein-cljsbuild "0.3.2"]
+  			[lein-cljsbuild "0.3.3"]
 
   			;; ring plugin
   			[lein-ring "0.8.7"]]
