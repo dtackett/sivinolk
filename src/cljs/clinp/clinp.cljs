022456eeaf01ebddf2c8796cb9345b680cc1e6f1
Devon Tackett <devon.tackett@gmail.com>
2013-09-27 22:41:49 -0700

    Basic keydown/keyup functionality

--- src/cljs/clinp/clinp.cljs
+++ src/cljs/clinp/clinp.cljs
@@ -1,24 +1,64 @@
 (ns clinp.core
-  (:require [clojure.browser.event :as event]))
+  (:require [clojure.browser.event :as event]
+            [goog.events.KeyCodes]))
+
+; TODO tests
+; test get-key-code for known key
+; test get-key-code for unknown key
+; test exec on non registered key handler
+; test exec on non registered phase
+; test exec on a register key handler and phase
+; keydown test false
+; keydown test true
+; setup
+; teardown
+; listen
+; listen same key, new phase
+; listen bad key
+; listen bad phase
+; listen overwrite
+; unlisten
+; unlisten bad key
+; unlisten bad phase
+; unlisten non-registered
 
 ; should follow the format of a map of key-code to a map of phases to a callback
-(def listeners (atom #{}))
+(def listeners (atom {}))
 
 (def downkeys (atom #{}))
 
-(def keymap {:left-arrow 37
-             :up-arrow 38
-             :right-arrow 39
-             :down-arrow 40})
+(defn get-key-code [key-name]
+  "Find the keyCode integer value for a keyword"
+  (aget js/goog.events.KeyCodes (name key-name)))
 
 (defn- exec [key-code phase]
   "Execute the function (if found) for the given bind key and phase"
-  (get (get listeners key-code) phase))
+  (let [handler (get (get @listeners key-code) phase)]
+    (if (not (nil? handler))
+      (handler))))
 
+; TODO Allow for an event listener target? Currently hard set to be the document body
 (defn setup []
   "Perform initial setup for input handler"
-  ; setup the google handler
-  (reset! downkeys #{}))
+  (do
+    (reset! downkeys #{})
+    ; Setup keydown handler
+    (event/listen
+      (.-body js/document)
+      "keydown"
+      (fn [event]
+        (do
+          (if (not (contains? @downkeys (.-keyCode event)))
+            (exec (.-keyCode event) :down))
+          (swap! downkeys conj (.-keyCode event)))))
+    ; Setup keyup handler
+    (event/listen
+      (.-body js/document)
+      "keyup"
+      (fn [event]
+          (if (contains? @downkeys (.-keyCode event))
+            (exec (.-keyCode event) :up))
+        (swap! downkeys disj (.-keyCode event))))))
 
 (defn teardown []
   "Perform any cleanup needed for input handler"
@@ -28,27 +68,24 @@
 
 (defn keydown? [test-key]
   "Test if the given key is currently held down."
-  (contains? @downkeys (get keymap test-key)))
+  (contains? @downkeys (get-key-code test-key)))
 
-(event/listen
-	(.-body js/document)
-	"keydown"
-  (fn [event]
-    (do
-; Beginning work on dispatching an initial event when the key is first pressed
-;      (if (not (contains? @downkeys (.-keyCode event)))
-;        (.log js/console (str "Keydown " (.-keyCode event))))
-      (swap! downkeys conj (.-keyCode event)))))
+; TODO The following event/listen should be inside the setup
 
-(event/listen
-	(.-body js/document)
-	"keyup"
-  (fn [event]
-    (swap! downkeys disj (.-keyCode event))))
 
 (defn listen [bind-key phase f]
   "Setup an callback for a given key and phase."
-  (swap! listeners assoc (get keymap bind-key) (assoc (get listeners (get keymap bind-key) #{}) phase f)))
+  ; TODO How to validate phase is in a known set?
+  ; TODO How to validate bind-key is known?
+  (let [key-code (get-key-code bind-key)]
+    (swap!
+     listeners
+     assoc
+     key-code
+     (assoc
+       (get @listeners key-code {})
+       phase
+       f))))
 
 (defn unlisten [bind-key phase]
   "Remove a callback for a given key and phase.")
@@ -56,9 +93,10 @@
 (defn pulse []
   "Trigger the pulse phase.")
 
-(def testmap {:b "1" :C "2"})
-(get testmap :C)
-
-(get :down-arrow keymap)
-
-(listen :down-arrow :down (fn [] (.log js/console "Down Pressed")))
+;(setup)
+;
+;(listen :UP :down (fn [] (.log js/console "Up Pressed")))
+;(listen :UP :up (fn [] (.log js/console "Up Released")))
+;
+;(listen :DOWN :down (fn [] (.log js/console "Down Pressed")))
+;(listen :DOWN :up (fn [] (.log js/console "Down Released")))
