886be0c8f88a65c51db8c986cd25ecb99b3184d5
Devon Tackett <devon.tackett@gmail.com>
2013-10-29 21:24:10 -0700

    Main game no longer starts as a side effect of evaluating source. Added explicit start function.

--- src/cljs/sivinolk/core.cljs
+++ src/cljs/sivinolk/core.cljs
@@ -6,18 +6,63 @@
             [sivinolk.entity :as entity]
             [sivinolk.world :as world]))
 
-; world-state works on the new entity system
+;; Create a main record to define the world
 (def world-state (atom {:next-id 0 :entities {}}))
 
-(swap! world-state pixi/setup-world)
-
-
+; Load some textures for entities
 (def bunny-texture (js/PIXI.Texture.fromImage "images/bunny.png"))
-
 (def ugly-block-texture (js/PIXI.Texture.fromImage "images/ugly-block.png"))
 
-;; TODO Create a function to remove an entity from the world
+; Hack to control which entity we are moving (this really should be part of the world state?)
+(def target-entity (atom 0))
 
+(defn select-next-controllable [world current-target]
+  "Select the next entity that has a controllable component. This is not smart enough to avoid infinite loops."
+  (loop [t (inc current-target)]
+    (let [newt (if (>= t (:next-id world)) 0 t)]
+      (if (:controllable (world/get-entity world newt))
+        newt
+        (recur (inc newt))))))
+
+
+(defn load-sample-world! []
+  (do
+    ;; Add a simple world block
+    (swap! world-state (fn [] (world/add-entity @world-state (entity/compose-entity
+                                                              [(comps/pixi-renderer. (js/PIXI.Sprite. ugly-block-texture))
+                                                               (comps/position. 100 190)
+                                                               (comps/aabb. 16 16)]))))
+
+    ; Add some test bunnies
+    (swap! world-state (fn [] (world/add-entity @world-state (entity/compose-entity
+                                                              [(comps/pixi-renderer. (js/PIXI.Sprite. bunny-texture))
+                                                               (comps/rotation. 0)
+                                                               (comps/velocity. 0 3)
+                                                               (comps/aabb. 26 37)
+                                                               (comps/controllable.)
+                                                               (comps/position. 100 100)]))))
+
+    (swap! world-state (fn [] (world/add-entity @world-state (entity/compose-entity
+                                                              [(comps/pixi-renderer. (js/PIXI.Sprite. bunny-texture))
+                                                               (comps/rotation. 0)
+                                                               (comps/velocity. 0 3)
+                                                               (comps/controllable.)
+                                                               (comps/position. 100 100)]))))
+
+    (swap! world-state (fn [] (world/add-entity @world-state (entity/compose-entity
+                                                              [(comps/pixi-renderer. (js/PIXI.Sprite. bunny-texture))
+                                                               (comps/rotation. 0)
+                                                               (comps/velocity. 10 4)
+                                                               (comps/controllable.)
+                                                               (comps/position. 100 100)]))))
+
+    (swap! target-entity #(select-next-controllable @world-state 0))
+    ))
+
+;; Scratch functions for playing with user input
+(defn simple-input-move! [world target-id x y]
+  (let [entity (world/get-entity @world target-id)]
+      (swap! world #(world/update-entity % (physics/move entity x y)))))
 
 (defn simple-add-entity! [world]
   (swap! world #(world/add-entity @world (entity/compose-entity
@@ -28,87 +73,40 @@
                          (comps/controllable.)
                          (comps/position. 100 100)]))))
 
-;; Create a main record to define the world
-; TODO: Stage should be provided and this whole thing should be built with a function
 
+; Setup for the pixi.js system
+(defn setup-pixi! [world]
+  (swap! world pixi/setup-world!))
 
+(defn setup-clinp! []
+  (do
+    ; clinp setup and keyboard handlers
+    (clinp/setup!)
 
-;; Add a simple world block
-(swap! world-state (fn [] (world/add-entity @world-state (entity/compose-entity
-                        [(comps/pixi-renderer. (js/PIXI.Sprite. ugly-block-texture))
-                         (comps/position. 100 190)
-                         (comps/aabb. 16 16)]))))
+    (clinp/listen! :Z :down
+                   (fn [] (swap! target-entity #(select-next-controllable @world-state %))))
 
-(swap! world-state (fn [] (world/add-entity @world-state (entity/compose-entity
-                        [(comps/pixi-renderer. (js/PIXI.Sprite. bunny-texture))
-                         (comps/rotation. 0)
-                         (comps/velocity. 0 3)
-                         (comps/aabb. 26 37)
-                         (comps/controllable.)
-                         (comps/position. 100 100)]))))
+    (clinp/listen! :X :down
+                   #((simple-add-entity! world-state)
+                     (swap! target-entity (fn [] (dec (:next-id @world-state))))))
 
-(swap! world-state (fn [] (world/add-entity @world-state (entity/compose-entity
-                        [(comps/pixi-renderer. (js/PIXI.Sprite. bunny-texture))
-                         (comps/rotation. 0)
-                         (comps/velocity. 0 3)
-                         (comps/controllable.)
-                         (comps/position. 100 100)]))))
+    (clinp/listen! :UP :pulse
+                   #(simple-input-move! world-state @target-entity 0 -6))
 
-(swap! world-state (fn [] (world/add-entity @world-state (entity/compose-entity
-                        [(comps/pixi-renderer. (js/PIXI.Sprite. bunny-texture))
-                         (comps/rotation. 0)
-                         (comps/velocity. 10 4)
-                         (comps/controllable.)
-                         (comps/position. 100 100)]))))
+    ;(clinp/listen! :DOWN :pulse
+    ;               #(simple-input-move! world-state @target-entity 0 2))
 
+    (clinp/listen! :LEFT :pulse
+                   #(simple-input-move! world-state @target-entity -3 0))
 
-(defn simple-input-move! [world target-id x y]
-  (let [entity (world/get-entity @world target-id)]
-      (swap! world #(world/update-entity % (physics/move entity x y)))))
-
-
-
-
-;(HACK-check-bound "x" (position. 110 10) {:x 100 :y 100})
-
+    (clinp/listen! :RIGHT :pulse
+                   #(simple-input-move! world-state @target-entity 3 0))
+    ))
 
 
 (defn update-world! [world]
                (clinp/pulse!)
                (swap! world #(physics/physics-system %)))
-
-; clinp setup and keyboard handlers
-(clinp/setup!)
-
-(defn- select-next-controllable [world current-target]
-  "Select the next entity that has a controllable component. This is not smart enough to avoid infinite loops."
-  (loop [t (inc current-target)]
-    (let [newt (if (>= t (:next-id world)) 0 t)]
-      (if (:controllable (world/get-entity world newt))
-        newt
-        (recur (inc newt))))))
-
-; Hack to control which entity we are moving
-(def target-entity (atom (select-next-controllable @world-state 0)))
-
-(clinp/listen! :Z :down
-              (fn [] (swap! target-entity #(select-next-controllable @world-state %))))
-
-(clinp/listen! :X :down
-               #((simple-add-entity! world-state)
-                 (swap! target-entity (fn [] (dec (:next-id @world-state))))))
-
-(clinp/listen! :UP :pulse
-               #(simple-input-move! world-state @target-entity 0 -6))
-
-;(clinp/listen! :DOWN :pulse
-;               #(simple-input-move! world-state @target-entity 0 2))
-
-(clinp/listen! :LEFT :pulse
-               #(simple-input-move! world-state @target-entity -3 0))
-
-(clinp/listen! :RIGHT :pulse
-               #(simple-input-move! world-state @target-entity 3 0))
 
 ;; setup animation loop
 (defn animate[]
@@ -118,4 +116,10 @@
   (pixi/render-system @world-state)
   (. (:renderer @world-state) render (:stage @world-state)))
 
-(js/requestAnimFrame animate)
+(defn start []
+  (do
+    (load-sample-world!)
+    (setup-clinp!)
+    (setup-pixi! world-state)
+    (js/requestAnimFrame animate)
+    ))
