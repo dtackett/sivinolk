87a4dc981aaec261f0b55a2e3b0041542d8333f3
Devon Tackett <devon.tackett@gmail.com>
2013-10-30 21:19:10 -0700

    Updated dependencies to latest versions.

--- project.clj
+++ project.clj
@@ -6,19 +6,19 @@
   :source-paths ["src/clj"]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1878"]
-                 [speclj "2.5.0"]
-                 [specljs "2.7.4"]
+                 [org.clojure/clojurescript "0.0-1978"]
+                 [speclj "2.8.0"]
+                 [specljs "2.8.0"]
                  [compojure "1.1.5"]]
 
   ;; lein-cljsbuild plugin to build a CLJS project
   :plugins [
             ;; cljsbuild plugin
-            [lein-cljsbuild "0.3.3"]
+            [lein-cljsbuild "0.3.4"]
 
             ;; speclj
-            [speclj "2.5.0"]
-            [specljs "2.7.4"]
+            [speclj "2.8.0"]
+            [specljs "2.8.0"]
 
             ;; ring plugin
             [lein-ring "0.8.7"]]
