be99a68509be2bce8177d2c9a768dacf11581433
Devon Tackett <devon.tackett@gmail.com>
2013-11-16 14:07:37 -0700

    Fixed where comments go

--- src/cljs/clinp/clinp.cljs
+++ src/cljs/clinp/clinp.cljs
@@ -13,12 +13,14 @@
 
 (def downkeys (atom #{}))
 
-(defn- get-key-code [key-name]
+(defn- get-key-code
   "Find the keyCode integer value for a keyword"
+  [key-name]
   (aget js/goog.events.KeyCodes (name key-name)))
 
-(defn- exec-handler! [listeners phase key-code]
+(defn- exec-handler!
   "Execute the function (if found) for the given bind key and phase"
+  [listeners phase key-code]
   (let [handler (get (get listeners key-code) phase)]
     (if (not (nil? handler))
       (handler))))
@@ -46,18 +48,21 @@
             (exec-handler! @listeners :up (.-keyCode event)))
         (swap! downkeys disj (.-keyCode event))))))
 
-(defn teardown! []
+(defn teardown!
   "Perform any cleanup needed for input handler"
+  []
   ; remove the google handler
   ; todo remove all current handlers
   (reset! downkeys #{}))
 
-(defn keydown? [test-key]
+(defn keydown?
   "Test if the given key is currently held down."
+  [test-key]
   (contains? @downkeys (get-key-code test-key)))
 
-(defn listen! [bind-key phase f]
+(defn listen!
   "Setup an callback for a given key and phase."
+  [bind-key phase f]
   ; TODO How to validate phase is in a known set?
   ; TODO How to validate bind-key is known?
   (let [key-code (get-key-code bind-key)]
@@ -71,8 +76,9 @@
        f))))
 
 ; TODO Implement this function
-(defn unlisten! [bind-key phase]
-  "Remove a callback for a given key and phase.")
+(defn unlisten!
+  "Remove a callback for a given key and phase."
+  [bind-key phase])
 
 (defn pulse!
   ([]
