bfaff22f8e76c24515df58ebf2ab09a01197e000
Devon Tackett <frogplague@gmail.com>
2014-05-06 15:16:21 -0700

    Added render cache to track what has been added to the stage so changes in the entity list can be disconnected from the renderer.

--- src/cljs/sivinolk/pixi.cljs
+++ src/cljs/sivinolk/pixi.cljs
@@ -2,6 +2,8 @@
 (ns sivinolk.pixi
   (:require [vyrlynd.world :as world]))
 
+; Keep an atom of the current cache of entities setup on the stage
+; This is really a shadow of what has been added to the stage
 (def render-cache (atom {}))
 
 ; Screen size should be defined elsewhere
@@ -13,8 +15,7 @@
     (do (.appendChild (.-body js/document) (.-view renderer))
       (-> world
           (assoc :stage stage)
-          (assoc :renderer renderer)
-          (assoc :render-cache {}))
+          (assoc :renderer renderer))
          )))
 
 ;; I feel like set-position and set-anchor could be made easier
@@ -48,14 +49,6 @@
              (assoc render-cache id sprite)))
          render-cache)))
 
-; Pixi system functions
-(defn ensure-entity-on-stage!
-  "Ensure the entity is on the given stage"
-  [entity stage]
-  (let [sprite (js/PIXI.Sprite. (:texture (:pixi-renderer entity)))
-        side-effect (. stage addChild sprite)]
-    (assoc entity :sprite sprite)))
-
 (defn update-display
   "Update the pixi-renderer component with the current state"
   [entity render-cache viewport]
@@ -68,6 +61,7 @@
     render-cache))
 
 (defn pixi-setup-entity
+  "Setup the given entity entry for display"
   [entity render-cache stage viewport]
   (if (:pixi-renderer entity)
     (update-display
@@ -75,21 +69,6 @@
        (cache-entity-on-stage! entity stage render-cache)
        viewport)
     render-cache))
-
-;; This is probably unsafe anymore, should be replaced or used in conjunction with
-;; a function that will clear up the entities in the game world as well.
-;; function to remove all elements from the stage
-(defn empty-stage
-  "Remove all Pixi DisplayObjects from the given stage."
-  [stage]
-  (doall (map
-   (fn [target]
-    ;; (.log js/console target)
-     (. stage removeChild target)
-     )
-   ;; do a slice 0 here to copy the array as the stage array mutates with removes
-   (.slice (.-children stage) 0))))
-
 (defn render-stage
   "This gets the pixi system to return and returns the world. Intended to make dealing with this a bit more composable."
   [world]
@@ -97,6 +76,7 @@
   world)
 
 (defn remove-from-stage
+  "Remove the given entity id from the cache and the stage"
   [render-cache stage entity-id]
   (do
     (if
@@ -107,39 +87,35 @@
 
 (defn reconsile-cache
   "This is intended to eventually removed entities that no longer exist from the pixi stage."
-  [world]
-  (assoc
-    world
-    :render-cache
-    (reduce
-     (fn [render-cache entity-id]
-       (cond
-;        (not (contains? (:entities world) entity-id))
-        :else
-        (remove-from-stage render-cache (:stage world) entity-id)
-        :else
-        render-cache
-        ))
-     (:render-cache world)
-     (keys (:render-cache world)))))
+  [world render-cache]
+  (reduce
+   (fn [render-cache entity-id]
+     (cond
+      (not (contains? (:entities world) entity-id))
+      (remove-from-stage render-cache (:stage world) entity-id)
+      :else
+      render-cache
+      ))
+   render-cache
+   (keys render-cache)))
 
 (defn setup-render-cache
-  [world]
+  [world render-cache]
   (let [viewport (:viewport (first (world/get-with-comp world :viewport)))]
-    (assoc
-      world
-      :render-cache
-      (reduce
-       (fn
-         [render-cache entity]
-         (pixi-setup-entity entity render-cache (:stage world) viewport))
-       (:render-cache world)
-       (vals (:entities world))))))
+    (reduce
+     (fn
+       [render-cache entity]
+       (pixi-setup-entity entity render-cache (:stage world) viewport))
+     render-cache
+     (vals (:entities world)))))
 
 ; pixi render system
 (defn render-system [world]
-  (-> world
-      reconsile-cache
-      setup-render-cache
-      render-stage))
+  (do
+    (swap! render-cache (fn [render-cache]
+                          (->> render-cache
+                               (reconsile-cache world)
+                               (setup-render-cache world))))
+    (-> world
+        render-stage)))
 
