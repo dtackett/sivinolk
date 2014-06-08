5eacdbb2b094fbb5c2a968d92a68e77c50d83526
Devon Tackett <frogplague@gmail.com>
2014-06-07 20:30:29 -0700

    Updated library version

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :source-paths ["src/clj"]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]
+                 [org.clojure/clojurescript "0.0-2227"]
                  [speclj "3.0.2"]
                  [compojure "1.1.5"]
                  [clinp "0.1.0-SNAPSHOT"]
@@ -15,7 +15,7 @@
   ;; lein-cljsbuild plugin to build a CLJS project
   :plugins [
             ;; cljsbuild plugin
-            [lein-cljsbuild "1.0.2"]
+            [lein-cljsbuild "1.0.3"]
 
             ;; speclj
             [speclj "3.0.2"]
